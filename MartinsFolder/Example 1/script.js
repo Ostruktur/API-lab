610e7559f1b2cba9c6f95ca8a60a831f24d09c29
Ostruktur <78792151+Ostruktur@users.noreply.github.com>
2021-03-11 18:53:18 +0100

    Monstera.csv
    playing around with what data to pull from Monstera.csv

--- MartinsFolder/Example 1/script.js
+++ MartinsFolder/Example 1/script.js
@@ -19,7 +19,7 @@ var svg = d3.select("#chart").append("svg").attr("width", diameter).attr("height
 
 //Here i will try to find an online plantbase API to tie into or try creating a more limited dataset on my own.
 d3.csv("Monstera.csv", function(error, data) {
-
+    console.log(data)
 _.each(data, function(element, index, list) {
     element.pop = +element.pop;
 });
@@ -64,7 +64,7 @@ function genJSON(csvData, groups) {
 // Unlike before, this controls the circles.
 // You don't change any other options.
 // This i will try to change to suit our concepts needs, plantbased database or writing my own
-var preppedData = genJSON(data, ['Family', 'Genus hybrid marker', 'Genus']);
+var preppedData = genJSON(data, ['Genus', 'Authorship', 'Taxonomic status in TPL']);
 
 
 
