def87aa586271a2e02b76adb4e0b15cf6c049a5f
Ostruktur <78792151+Ostruktur@users.noreply.github.com>
2021-03-12 09:44:09 +0100

    update
    changing styule and looking at mouseover event

--- MartinsFolder/Prototype example/script.js
+++ MartinsFolder/Prototype example/script.js
@@ -29,15 +29,11 @@ var pubs =
                 {"name": "AUT-21"},
                 {"name": "AUT-22"},
                 {"name": "AUT-23"},
-                {"name": "AUT-24"},
-                {"name": "AUT-25"},
                 {"name": "AUT-26"},
                 {"name": "AUT-27"},
                 {"name": "AUT-28","children":[
                     {"name": "AFF-281"},
                     {"name": "AFF-282"},
-                    {"name": "AFF-283"},
-                    {"name": "AFF-284"},
                     {"name": "AFF-285"},
                     {"name": "AFF-286"}
                 ]}
@@ -50,8 +46,6 @@ var pubs =
                 {"name": "AUT-42"},
                 {"name": "AUT-43","children": [
                     {"name": "AFF-431"},
-                    {"name": "AFF-432"},
-                    {"name": "AFF-433"},
                     {"name": "AFF-434","children":[
                         {"name": "ADD-4341"},
                         {"name": "ADD-4342"},
@@ -65,7 +59,6 @@ var pubs =
                 {"name": "M.Smith 5/11-1978","children":[
                     {"name": "J.Doe 12/4 1979"},
                     {"name": "H.Unter 25/5 1979"},
-                    {"name": "E.Nigel 1/6 1979"},
                     {"name": "A.Hunter 4/4 1980"},
                     {"name": "M.Thorn 17/8 1980"},
                     {"name": "E.West 19/11 1981"}
@@ -86,8 +79,6 @@ var pubs =
             "name": "Events","children": [
               {"name": "AUT-61","children":[
                   {"name": "AFF-611"},
-                  {"name": "AFF-612"},
-                  {"name": "AFF-613"},
                   {"name": "AFF-614","children":[
                       {"name": "ADD-6141"},
                       {"name": "ADD-6142"},
@@ -97,33 +88,30 @@ var pubs =
               {"name": "AUT-63"},
               {"name": "AUT-64"},
               {"name": "AUT-65"},
-              {"name": "AUT-66"},
-              {"name": "AUT-67"},
-              {"name": "AUT-68"},
-              {"name": "AUT-69"}
+             
             ]
         }
     ]
 };
 //Usable space
-var diameter = 1400;
+let diameter = 1400;
 
-var margin = {top: 20, right: 120, bottom: 20, left: 120},
+let margin = {top: 20, right: 120, bottom: 20, left: 120},
     width = diameter,
     height = diameter;
     
-var i = 0,
+let i = 0,
     duration = 350,
     root;
 
-var tree = d3.layout.tree()
+let tree = d3.layout.tree()
     .size([270, diameter / 2 - 10])
     .separation(function(a, b) { return (a.parent == b.parent ? 1 : 3) / a.depth; });
 
-var diagonal = d3.svg.diagonal.radial()
+let diagonal = d3.svg.diagonal.radial()
     .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
 
-var svg = d3.select("body").append("svg")
+let svg = d3.select("body").append("svg")
     .attr("width", width )
     .attr("height", height )
   .append("g")
@@ -141,18 +129,18 @@ d3.select(self.frameElement).style("height", "1400px");
 function update(source) {
 
   // Calculate new tree layout.
-  var nodes = tree.nodes(root),
+  let nodes = tree.nodes(root),
       links = tree.links(nodes);
 
   // Normalize for fixed-depth.
   nodes.forEach(function(d) { d.y = d.depth * 200; });
 
   // Updating nodes
-  var node = svg.selectAll("g.node")
+  let node = svg.selectAll("g.node")
       .data(nodes, function(d) { return d.id || (d.id = ++i); });
 
   // Enter any new nodes at the parent's previous position.
-  var nodeEnter = node.enter().append("g")
+  let nodeEnter = node.enter().append("g")
       .attr("class", "node")
       .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
       .on("click", click);
@@ -174,7 +162,7 @@ function update(source) {
 
 
   // Transition nodes to their new position.
-  var nodeUpdate = node.transition()
+  let nodeUpdate = node.transition()
       .duration(duration)
       .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
 
@@ -187,7 +175,7 @@ function update(source) {
       .attr("transform", function(d) { return d.x < 180 ? "translate(0)" : "rotate(180)translate(-" + (d.name.length + 50)  + ")"; });
 
   //Transitions i'm not really sure about, will try some different stuff
-  var nodeExit = node.exit().transition()
+  let nodeExit = node.exit().transition()
       .duration(duration)
       //.attr("transform", function(d) { return "diagonal(" + source.y + "," + source.x + ")"; })
       .remove();
@@ -199,7 +187,7 @@ function update(source) {
       .style("fill-opacity", 1e-6);
 
   // Update the links…
-  var link = svg.selectAll("path.link")
+  let link = svg.selectAll("path.link")
       .data(links, function(d) { return d.target.id; });
 
   // Enter any new links at the parent's previous position.
